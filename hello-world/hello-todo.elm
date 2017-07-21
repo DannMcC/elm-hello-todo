adeb182a5867ce3d10227241d01eae7fa1a0264b
Dan McCune <dmccune@hivelocity.net>
2017-07-21 10:13:59 -0400

    hello hello-todo

---
+++ hello-world/hello-todo.elm
@@ -0,0 +1,15 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+main =
+    view
+
+{-Model-}
+{-Update-}
+{-View-}
+view =
+    div []
+        [ input [ type' "text" ] []
+        , button [] [ text "Add Todo" ]
+        ]
+
