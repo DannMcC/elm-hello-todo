bbfeef4a891f7cb2fe2be20c26c7241ad0ad44a1
Dan McCune <dmccune@hivelocity.net>
2017-07-21 15:52:59 -0400

    ...

--- hello-world/hello-todo.elm
+++ hello-world/hello-todo.elm
@@ -1,15 +1,44 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Html as Html
 
 main =
-    view
-
+    Html.beginnerProgram
+        { model = model
+        , update = update
+        , view = view
+        }
 {-Model-}
+
+type alias Model =
+    { todo: String
+    , tacos: List String
+    }
+
+model =
+    { todo = ""
+    , tacos = []
+    }
 {-Update-}
+type Msg = UpdateText String
+
+update msg model =
+    case msg of
+        UpdateText text ->
+            { model | todo = text }
+
+        AddItem ->
+            { model | tacos = model.todo :: model.tacos }
+
 {-View-}
-view =
+view model =
     div []
-        [ input [ type' "text" ] []
-        , button [] [ text "Add Todo" ]
+        [ input [ kind "text"
+            , onInput UpdateText
+            , value model.todo
+            ] []
+        , button [ onClick AddItem ] [ text "Add Todo" ]
+        , div [] [ text model.todo ]
         ]
 
